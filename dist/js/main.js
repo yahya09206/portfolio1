00478ab559832d4999d48d60c3858d2bbdb898d5
Yahya Hussein <yahyahussein91@gmail.com>
2019-07-24 17:54:24 -0700

    finished with js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,32 @@
 // Select DOM Items
-const menuBtn = document.querySelector('.btn-menu');
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+
+const navItems = document.querySelectorAll('.nav-item');
+
+//Set initial state of menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+	if(!showMenu){
+		menuBtn.classList.add('close');
+		menu.classList.add('show');
+		menuNav.classList.add('show');
+		menuBranding.classList.add('show');
+		navItems.forEach(item => item.class.add('show'));
+		// Set menu state
+		showMenu = true;
+	}else {
+		menuBtn.classList.remove('close');
+		menu.classList.remove('show');
+		menuNav.classList.remove('show');
+		menuBranding.classList.remove('show');
+		navItems.forEach(item => item.class.remove('show'));
+		// Set menu state
+		showMenu = false;
+	}
+}
