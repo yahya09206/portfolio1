57d0e9bf0e621e37e53cf1f6f4c7e4560806cded
Yahya Hussein <yahyahussein91@gmail.com>
2019-07-24 18:18:36 -0700

    fixed java error

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,7 +17,7 @@ function toggleMenu() {
 		menu.classList.add('show');
 		menuNav.classList.add('show');
 		menuBranding.classList.add('show');
-		navItems.forEach(item => item.class.add('show'));
+		navItems.forEach(item => item.classList.add('show'));
 		// Set menu state
 		showMenu = true;
 	}else {
@@ -25,7 +25,7 @@ function toggleMenu() {
 		menu.classList.remove('show');
 		menuNav.classList.remove('show');
 		menuBranding.classList.remove('show');
-		navItems.forEach(item => item.class.remove('show'));
+		navItems.forEach(item => item.classList.remove('show'));
 		// Set menu state
 		showMenu = false;
 	}
